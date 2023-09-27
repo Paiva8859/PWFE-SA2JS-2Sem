7f53daba1934e24a1b4a8e8eea34a5fc31255314
Paiva8859 <gabrielaguilarpaiva@gmail.com>
2023-09-27 14:05:56 -0300

    a

--- js/lampada.js
+++ js/lampada.js
@@ -16,28 +16,20 @@ function isLampBroken() {
 function lampOn() {
     if (!broken) {
         lamp.src = '../img/ligada.png';
-        alavanca.src = '../img/alavancaLigada.png';
-        redstone.src = '../img/RedAcesa.png';
-        alavanca.style.height = '30vh';
-    } else {
-        alavanca.src = '../img/alavancaLigada.png';
-        redstone.src = '../img/RedAcesa.png';
-        alavanca.style.height = '30vh';
     }
+    alavanca.src = '../img/alavancaLigada.png';
+    redstone.src = '../img/RedAcesa.png';
+    alavanca.style.height = '30vh';
 }
 
 // Função para desligar a lâmpada
 function lampOff() {
     if (!broken) {
         lamp.src = '../img/desligada.png';
-        alavanca.src = '../img/alavancaDesligada.png';
-        redstone.src = '../img/RedApagada.png';
-        alavanca.style.height = '40vh';
-    } else {
-        alavanca.src = '../img/alavancaDesligada.png';
-        redstone.src = '../img/RedApagada.png';
-        alavanca.style.height = '40vh';
     }
+    alavanca.src = '../img/alavancaDesligada.png';
+    redstone.src = '../img/RedApagada.png';
+    alavanca.style.height = '40vh';
 }
 
 // Função para quebrar a lâmpada
@@ -59,14 +51,10 @@ lamp.addEventListener('dblclick', lampBroken);
 
 // Evento de clique na alavanca para ligar ou desligar a lâmpada
 alavanca.addEventListener('click', function () {
-    if (broken) {
-        // Se a lâmpada estiver quebrada, não faz nada
-        return;
-    }
-    if (lamp.src.endsWith('desligada.png')) {
-        lampOn();
-    } else {
+    if (alavanca.src.endsWith('alavancaLigada.png')) {
         lampOff();
+    } else {
+        lampOn();
     }
 });
 
@@ -76,3 +64,5 @@ bau.addEventListener('click', function () {
         lampFix();
     }
 });
+
+
